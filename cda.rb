95287c983467da13a46e5bf766f7eba430a54fea
Jake Runzer <jakerunzer@gmail.com>
2017-11-16 16:44:13 -0800

    update hash

--- cda.rb
+++ cda.rb
@@ -2,7 +2,7 @@ class Cda < Formula
   desc "Create and manage change directory aliases."
   homepage "https://github.com/coffee-cup/cda"
   url "https://artifacts.jakerunzer.com/cda/1.0.0/cda.tar.gz"
-  sha256 "7e75f7fac3772b93c60c96783cb6d0a92aaff702edd347438cc29d72dcb58b7e"
+  sha256 "a0b86e22c9012a464219a9e664483d90b7c3fed90c413c8eb2088f31ed30289e"
   version "1.0.0"
 
   bottle :unneeded
