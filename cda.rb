2392680f22bae3a76585a6294d2f83eb0f83b9e4
Jake Runzer <jakerunzer@gmail.com>
2020-01-16 15:49:42 +0000

    update cda url

--- cda.rb
+++ cda.rb
@@ -1,7 +1,7 @@
 class Cda < Formula
   desc "Create and manage change directory aliases."
   homepage "https://github.com/coffee-cup/cda"
-  url "https://artifacts.jakerunzer.com/cda/1.0.0/cda.tar.gz"
+  url "https://github.com/coffee-cup/cda/releases/download/1.0.0/cda"
   sha256 "a0b86e22c9012a464219a9e664483d90b7c3fed90c413c8eb2088f31ed30289e"
   version "1.0.0"
 
